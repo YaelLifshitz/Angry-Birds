de2ac6ec450c0286eecff2daac64e753d3374849
YaelLifshitz <64844458+YaelLifshitz@users.noreply.github.com>
2020-05-08 15:27:02 +0300

    moving the images to an img file creating a file for audio segmentation working on the game - when to bullet touch a bird the bird dies.

--- my_game.py
+++ my_game.py
@@ -2,19 +2,28 @@ import pygame
 from random import randint
 from shapes import BlackBird
 from shapes import BlueBird
+from shapes import Plain
 from shapes import Bullet
+import os
+pygame.init()
+# Set up an assets folders, so the game could run on any computer
+Angry_Birds_Game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(Angry_Birds_Game_folder, "img")
+sound_folder = os.path.join(Angry_Birds_Game_folder, "sound")
 
+# Some constant var
 WINDOW_WIDTH = 802
 WINDOW_HEIGHT = 466
 PINK_BACKGROUND = (255, 20, 147)
 FPS = 60 # frames per sec
 SOUND_FILE = "Angry Birds Theme Song.mp3"
-IMAGE_BACKGROUND = 'clouds background.jpg' # background game image
+
 
 # setting of mouse movement:
 LEFT = 1
 SCROLL = 2
 RIGHT = 3
+
 
 ## initialzing pygame and creating a window
 
@@ -23,7 +32,7 @@ pygame.init()
 size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Angry Birds Game")
-img_background = pygame.image.load(IMAGE_BACKGROUND)
+img_background = pygame.image.load(os.path.join(img_folder, 'clouds background.jpg')).convert()
 screen.blit(img_background, (0, 0))  # loading the image to coordinates (0,0)
 # if we load to (150,0) the image will move more to the right on the screen
 # so the bottom rightest pixel on the screen will be (801, 465)
@@ -31,7 +40,7 @@ screen.blit(img_background, (0, 0))  # loading the image to coordinates (0,0)
 # init player- the plain
 pygame.mouse.set_visible(False) # in order to not see the mouse arrow
 
-player_image = pygame.image.load('plain.png').convert()
+player_image = pygame.image.load(os.path.join(img_folder, 'plain.png')).convert()
 player_image.set_colorkey(PINK_BACKGROUND)
 screen.blit(player_image, [220, 300]) #the size of the plain
 pygame.display.flip()
@@ -41,12 +50,14 @@ clock = pygame.time.Clock()
 
 # init music
 pygame.mixer.init()
-pygame.mixer.music.load(SOUND_FILE)
+pygame.mixer.music.load(os.path.join(sound_folder, "Angry Birds Theme Song.mp3"))
 pygame.mixer.music.play(loops=-1)
 
 
 all_bullets = pygame.sprite.Group()
 all_birds = pygame.sprite.Group()
+player = Plain()
+pygame.time.set_timer(pygame.USEREVENT, 500)
 ## Game loop:
 finish = False
 while not finish:
@@ -58,11 +69,20 @@ while not finish:
         # Check if user closed the window
         if event.type == pygame.QUIT:
             finish = True
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
-            x, y = pygame.mouse.get_pos()
-            mouse_point = pygame.mouse.get_pos()
-            screen.blit(player_image, mouse_point)
+        # timing the birds creation
+        if event.type == pygame.USEREVENT:
+            random_choose = randint(1, 2)
+            y = randint(0, 400)
+            if random_choose == 1:
+                bird = BlackBird(-5, y)
+            else:
+                bird = BlueBird(-5, y)
+            all_birds.add(bird)
 
+
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
+            x, y = player.get_pos()
             # Creating the bullets
             bullet = Bullet(x, y)
             all_bullets.add(bullet)
@@ -70,21 +90,11 @@ while not finish:
             pygame.display.flip()
 
         else:
-            x, y = pygame.mouse.get_pos()
+            mouse_point = pygame.mouse.get_pos()
+            player.update_loc_mouse(mouse_point)
 
     # In order to not leaving marks on background from moving objects
     screen.blit(img_background, (0, 0))
-
-
-    y1 = randint(0, 400)
-    y2 = randint(0, 400)
-    bird = BlackBird(0, y1)
-    all_birds.add(bird)
-    bird = BlueBird(0, y2)
-    all_birds.add(bird)
-
-
-    all_birds.draw(screen)
 
 
 
@@ -93,17 +103,31 @@ while not finish:
     for bird in all_birds:
         bird.update_loc()
         screen.blit(bird.image, bird.get_pos())
-        #pygame.display.flip()
 
 
     for bullet in all_bullets:
         bullet.update_loc()
         screen.blit(bullet.image, bullet.get_pos())
+        # Checking if a bullet hits a bird and if so it will delete the bird
+        bullet_hits_birds_list = pygame.sprite.spritecollide(bullet, all_birds, True)
+
+    # Checking if a bullet hits a bird and if so it will delete the bird
+    if pygame.sprite.spritecollide(player, all_birds):
+        birds_hits_player_list = pygame.sprite.spritecollide(player, all_birds, True)
+        pygame.mixer.music.load(os.path.join(sound_folder, "Lose , Losing Sound Effects All Sounds.mp3"))
+        pygame.mixer.music.play()
+
+    print(birds_hits_player_list)
+
+    # Draw / render
+    screen.blit(player.image, player.get_pos())
 
 
-    mouse_point = pygame.mouse.get_pos()
-    screen.blit(player_image, mouse_point)
-    pygame.display.flip()
+    # bullet_hits_birds_list = pygame.sprite.spritecollide(all_bullets, all_birds, True)
+
+    # mouse_point = pygame.mouse.get_pos()
+    # screen.blit(player_image, mouse_point)  # In order to show the plain
+
 
 pygame.quit()
 
--- shapes.py
+++ shapes.py
@@ -1,19 +1,64 @@
 import pygame
 from random import randint
+import os
+Angry_Birds_Game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(Angry_Birds_Game_folder, "img")
+#player_image = pygame.image.load(os.path.join(img_folder, 'plain.png')).convert()
+
+
+class Plain(pygame.sprite.Sprite):
+    # the constructor/ sprite for the Plain
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.image = pygame.image.load(os.path.join(img_folder, 'plain.png')).convert()
+        PINK_BACKGROUND = (255, 20, 147)
+        self.image.set_colorkey(PINK_BACKGROUND)
+        # Every image has a rectangle around it- the image size - the get_rect function gives us that
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = (0, 0)
+        self.speedx = 0
+        self.speedy = 0
+
+
+    # the next few methodes rule is to find the birds locations each time we want
+    def update_loc_mouse(self, mouse_point):
+        self.rect.x, self.rect.y = mouse_point
+        if self.rect.x < 600:
+            self.rect.x = 600
+        elif self.rect.x > 790:
+            self.rect.x = 790
+
+    # def update_loc_k(self):
+    #     self.speedx = 0
+    #     self.speedy = 0
+    #     keystate = pygame.key.get_pressed()
+    #     if keystate[pygame.K_LEFT]:
+    #         self.speedx = -5
+    #     if keystate[pygame.K_RIGHT]:
+    #         self.speedx = 5
+    #     self.rect.x += self.speedx
+    #     if keystate[pygame.K_UP]:
+    #         self.speedy = -5
+    #     if keystate[pygame.K_DOWN]:
+    #         self.speedy = +5
+    #     self.rect.y += self.speedy
+
+    def get_pos(self):
+        return self.rect.x, self.rect.y
 
 
 class BlueBird(pygame.sprite.Sprite):
     # the constructor/ sprite for the blue bird
     def __init__(self, x, y):
         super(self.__class__, self).__init__()
-        self.image = pygame.image.load('angry bird blue.png').convert()
+        self.image = pygame.image.load(os.path.join(img_folder, 'angry bird blue.png')).convert()
         PINK_BACKGROUND = (255, 20, 147)
         self.image.set_colorkey(PINK_BACKGROUND)
         # Every image has a rectangle around it- the image size - the get_rect function gives us that
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.__vx = randint(0, 10)
+        self.__vx = randint(1, 10)
         self.__vy = randint (-10, 10)
 
     # the next few methodes rule is to find the birds locations each time we want
@@ -36,13 +81,13 @@ class BlackBird(pygame.sprite.Sprite):
     # the constructor
     def __init__(self, x, y):
         super(self.__class__, self).__init__()
-        self.image = pygame.image.load('angry bird black.png').convert()
+        self.image = pygame.image.load(os.path.join(img_folder, 'angry bird black.png')).convert()
         PINK_BACKGROUND = (255, 20, 147)
         self.image.set_colorkey(PINK_BACKGROUND)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.__vx = randint(0, 10)
+        self.__vx = randint(1, 10)
         self.__vy = randint(-10, 10)
 
     # the next few methodes rule is to find the birds locations each time we want
@@ -66,7 +111,7 @@ class Bullet(pygame.sprite.Sprite):
     # the constructor
     def __init__(self, x, y):
         super(self.__class__, self).__init__()
-        self.image = pygame.image.load('bullet.png').convert()
+        self.image = pygame.image.load(os.path.join(img_folder, 'bullet.png')).convert()
         PINK_BACKGROUND = (255, 20, 147)
         self.image.set_colorkey(PINK_BACKGROUND)
         self.rect = self.image.get_rect()
